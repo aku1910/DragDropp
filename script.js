40e8492001679264852ae5cbd6db3ea51a6aace6
aku1910 <154792762+aku1910@users.noreply.github.com>
2024-02-20 01:12:05 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const fileInput = document.getElementById("fileInput");
+fileInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    function readFile(file) {
+        reader.onload = (e) => {
+            const box1 = document.getElementById("box1");
+
+            const fileExtension = file.name.split(".").pop().toLowerCase();
+
+            if (["jpg", "jpeg", "png", "s"].includes(fileExtension)) {
+                const img = new Image();
+                img.src = e.target.result;
+                box1.innerHTML = "";
+                img.className = "dragable";
+                box1.appendChild(img);
+                img.setAttribute("id", "imgdrag");
+
+                img.addEventListener("dragstart", (e) => {
+                    e.dataTransfer.setData("image", e.target.id);
+                });
+            } else {
+                alert("Supported file formats: jpg, jpeg, png, s");
+            }
+        };
+
+        if (file.type.startsWith("image/")) {
+            reader.readAsDataURL(file);
+        } else {
+            alert("Unsupported file type. Please select an image.");
+        }
+    }
+
+    readFile(file);
+});
+
+const container = document.querySelectorAll(".box");
+
+container.forEach((box) => {
+    box.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+    box.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const data = e.dataTransfer.getData("image");
+        const dragelement = document.getElementById(data);
+        console.log(data);
+        box.innerHTML = "";
+        box.append(dragelement);
+    });
+});
